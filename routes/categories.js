58e26a60b42c9ddffde10073fe8bf6860fe88081
Danaka99 <140838401+Danaka99@users.noreply.github.com>
2024-11-06 10:14:04 +0530

    modify

--- routes/categories.js
+++ routes/categories.js
@@ -14,11 +14,36 @@ cloudinary.config({
 
 // Get all categories
 router.get('/', async (req, res) => {
-    const categoryList = await Category.find();
+
+    try{
+    const page = parseInt(req.query.page) || 1;
+    const perPage = 5;
+    const totalPosts = await Category.countDocuments();
+    const totalPages = Math.ceil(totalPosts/perPage);
+
+    if (page > totalPages){
+        return res.status(404).json({message: "Page Not Found"})
+    }
+
+    const categoryList = await Category.find()
+     .skip((page - 1) * perPage)
+     .limit(perPage)
+     .exec();
+
     if (!categoryList) {
         return res.status(500).json({ success: false });
     }
-    res.send(categoryList);
+
+    return res.status(200).json({
+        "categoryList":categoryList,
+        "totalPages":totalPages,
+        "page":page
+    });
+    
+    }catch(error){
+        res.status(500).json({success:false})
+    }
+   
 });
 
 // Get category by ID
